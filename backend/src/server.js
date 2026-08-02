d446207c3b44e18846f083a1e73fb7303131e0f8
shifa-m <shifamahreen786@gmail.com>
2026-08-02 20:33:09 +0530

    frontend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,33 @@
 import express from "express"
 import connectDB from "./config/db.js"
+import dotenv from  "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
+import authRoutes from "./features/auth/auth.routes.js"
+import categoryRoutes from "./features/categories/category.routes.js"
+import dashboardRoutes from "./features/dashboard/dashboard.routes.js"
+import expensesRoutes from "./features/expenses/expenses.routes.js"
 
+dotenv.config()
 connectDB()
 const app=express()
 
 
 app.use(express.json())
+app.use(cookieParser())
 
+app.use("/api/auth",authRoutes)
+app.use("/api/categories",categoryRoutes)
+app.use("/api/expenses",expensesRoutes)
+app.use("/api/dashboard",dashboardRoutes)
+
+app.use((err,res,req,next)=>{
+            console.log(err.stack)
+            res.status(500).json({
+                        message:"Something went wong"
+            })
+})
 
 
 const PORT=3000
